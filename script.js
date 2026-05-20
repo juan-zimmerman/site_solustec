3e54e77ae881c11ff4f2078ec3a34423722e0984
juan-zimmerman <juanito.tevez@gmail.com>
2026-05-20 10:09:15 -0300

    melhoria pag produtos e serviços

--- script.js
+++ script.js
@@ -14,6 +14,58 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
+    document.getElementById("lightbox")
+.addEventListener("click", function(e){
+
+    if(e.target.id==="lightbox"){
+        fecharGaleria();
+    }
+
+});
+const galerias = [
+  [
+    "./imagens/Caso1Original.png",
+    "./imagens/Caso1Realizado.png"
+  ],
+  [
+    "./imagens/caso2antes.jpeg",
+    "./imagens/caso2depois.jpeg"
+  ],
+  [
+    "./imagens/caso3-antes.jpg",
+    "./imagens/caso3-depois.jpg"
+  ]
+];
+
+let galeriaAtual = 0;
+let fotoAtual = 0;
+
+window.abrirGaleria = function(index) {
+  galeriaAtual = index;
+  fotoAtual = 0;
+
+  document.getElementById("fotoLightbox").src = galerias[galeriaAtual][fotoAtual];
+  document.getElementById("lightbox").classList.add("ativo");
+};
+
+window.fecharGaleria = function() {
+  document.getElementById("lightbox").classList.remove("ativo");
+};
+
+window.trocarFoto = function(direcao) {
+  fotoAtual += direcao;
+
+  if (fotoAtual < 0) {
+    fotoAtual = galerias[galeriaAtual].length - 1;
+  }
+
+  if (fotoAtual >= galerias[galeriaAtual].length) {
+    fotoAtual = 0;
+  }
+
+  document.getElementById("fotoLightbox").src = galerias[galeriaAtual][fotoAtual];
+};
+
     // ANIMAÇÃO AO SCROLL
     const elementos = document.querySelectorAll('.reveal');
 
